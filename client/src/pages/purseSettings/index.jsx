af043bf382f29f994623b1f21583d7a6365c71ed
JeremiahAfriprud <87825568+JeremiahAfriprud@users.noreply.github.com>
2022-01-15 17:47:40 +0100

    setup purse setting Page

--- client/src/pages/purseSettings/index.jsx
+++ client/src/pages/purseSettings/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const PurseSettings = () => {
-    return (
-        <div>
-            Purse settings page
-        </div>
-    )
-}
+  return (
+    <div className="w-full bg-dark-1 h-3/4 px-8">
+      <p className="text-8xl text-white-1">Purse settings page</p>
+    </div>
+  );
+};
 
-export default PurseSettings
+export default PurseSettings;
