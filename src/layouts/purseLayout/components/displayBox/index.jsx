677f4bc135e7ae4bb813b557642c431cf6d99388
Jeremiah Oyebamiji <phemmy09@gmail.com>
2022-03-28 11:22:19 +0100

    fixes

--- src/layouts/purseLayout/components/displayBox/index.jsx
+++ src/layouts/purseLayout/components/displayBox/index.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const DisplayBox = ({ icon, name, desc, theme }) => {
 
   return (
-    <div className={`h-auto md:h-28 ${theme === 'dark'?'bg-dark-2': 'bg-white-1' } p-2 md:p-8 rounded-2xl flex items-center border-purple-1 border`}>
-      <div className="">
+    <div className={`h-auto md:h-28 ${theme === 'dark'?'bg-dark-2': 'bg-white-1' } p-2 md:p-8 rounded-2xl sm:flex items-center border-purple-1 border`}>
+      <div className="flex justify-center">
         <div>
           <img
             src={icon}
             alt="coin"
-            className="img-responsive"
+            className="sm:w-auto sm:img-responsive"
           />
         </div>
       </div>
-      <div className="ml-4 flex flex-col items-center">
+      <div className="sm:ml-4 flex flex-col items-center">
         <p className={` ${theme === 'dark'?'text-white-1' : 'text-dark-1'} Poppins text-center text-xs md:text-xl`}>{name}</p>
-        <p className={`${theme === 'dark'?'text-white-1' : 'text-dark-1'} Poppins text-xs font-light`}>{desc}</p>
+        <p className={`${theme === 'dark'?'text-white-1' : 'text-dark-1'} Poppins text-center text-xs font-light`}>{desc}</p>
       </div>
     </div>
   );
--- src/pages/purse/components/Details.js
+++ src/pages/purse/components/Details.js
@@ -4,12 +4,12 @@ function Details({theme}) {
     return (
         <React.Fragment>
             <div className="flex flex-col items-center">
-                <div className="h-32 w-32 bg-indigo-700 rounded-full text-white-1 flex justify-center items-center mt-16">
+                <div className="h-32 w-32 bg-members-gradient rounded-full text-white-1 flex justify-center items-center mt-16">
                     <div className="h-28 w-28 bg-gray-16 rounded-full"></div>
                 </div>
-                <div className="text-white-1 mt-12 flex justify-between lg:w-2/5">
-                    <p className={`Poppins ${theme === 'dark'? 'text-white-1' : 'text-dark-1'} font-medium text-sm`}>Available Members</p>
-                    <p className={`Poppins ${theme === 'dark'? 'text-white-1' : 'text-dark-1'} font-medium text-sm`}>Members Expected</p>
+                <div className="text-white-1 mt-12 flex justify-between w-full lg:w-2/5">
+                    <p className={`Poppins ${theme === 'dark'? 'text-white-1' : 'text-dark-1'} font-medium text-xs md:text-sm`}>Available Members</p>
+                    <p className={`Poppins ${theme === 'dark'? 'text-white-1' : 'text-dark-1'} font-medium text-xs md:text-sm`}>Members Expected</p>
                 </div>
                 <div className="mt-8">
                     <p className={`Poppins ${theme === 'dark'? 'text-white-1' : 'text-dark-1'} font-semibold text-lg text-center`}>
