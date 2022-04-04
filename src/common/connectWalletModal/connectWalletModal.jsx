cdbb692b6afd55bf9badf03ccca8c371c8a9aa85
AjayiMike <ajayimikeofficial@gmail.com>
2022-04-04 18:52:06 +0100

    add a notification package and display notification based on what happens when user connect

--- src/common/connectWalletModal/connectWalletModal.jsx
+++ src/common/connectWalletModal/connectWalletModal.jsx
@@ -1,82 +1,100 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CgCloseR } from "react-icons/cg";
 import { connectorsData } from "../../web3";
 import { useWeb3React } from "@web3-react/core";
 import { getConnectionError } from "../../web3";
+import { useToasts } from "react-toast-notifications";
 import clsx from "clsx";
 
 const ConnectWalletModal = ({ onClose }) => {
+    const { active, activate, connector } = useWeb3React();
+    const { addToast } = useToasts();
 
-  const { activate } = useWeb3React();
+    // handle logic to recognize the connector currently being activated
+    // const [activatingConnector, setActivatingConnector] = useState(undefined);
 
-  // handle logic to recognize the connector currently being activated
-  // const [activatingConnector, setActivatingConnector] = useState(undefined);
+    const connectWallet = (connector) => {
+        // setActivatingConnector(JSON.stringify(connector));
+        activate(connector, handleError);
+    };
 
-  const connectWallet = (connector) => {
-    // setActivatingConnector(JSON.stringify(connector));
-    activate(connector, handleError);
-  };
+    const handleError = (err) => {
+        const errorString = getConnectionError(err);
+        addToast(errorString, { appearance: "error" });
+        onClose();
+    };
 
-  const handleError = (err) => {
-    const errorString = getConnectionError(err);
-    // the errorString will be used to triger error snackbar
-    console.log("web3React Error: ", errorString);
-  };
+    useEffect(() => {
+        if (active) {
+            addToast("Wallet connected", {
+                appearance: "success",
+            });
+            onClose();
+        }
+    }, [active]);
 
-  return (
-    <>
-      <div className="flex justify-between items-center text-white-1 font-semibold text-xl">
-        <h2 className="dark:text-white-1 text-dark-1">Connect Wallet</h2>
-        <CgCloseR className="cursor-pointer dark:text-white-1 text-dark-1" onClick={onClose} />
-      </div>
-      <div className="">
-        <div
-          className={`text-gray-6 p-2 rounded md:rounded-xl w-full mt-2 mb-4 text-left`}
-        >
-          <span className="text-base md:text-base">
-            <span>By connecting a wallet, you have agreed to chained thrift </span>
-            <span>
-              <a href="#!" className="text-blue-1">
-                terms of service
-              </a>
-            </span>
-          </span>
-        </div>
-        {connectorsData.map((connectorObj, idx) => {
-          const { name, connector, icon: Icon } = connectorObj;
-          return (
-            <button
-              key={idx}
-              className={clsx({
-                "bg-white-1 dark:bg-gray-5 text-white-1 p-2 rounded md:rounded-xl block w-full mt-4 text-left align-middle": true,
-                "opacity-60 cursor-not-allowed": false,
-              })}
-              onClick={() => connectWallet(connector)}
-            >
-              <Icon width="35px" className="inline-block mr-6" />
-              <span
-                className={`inline-block text-base dark:text-white-1 text-dark-1`}
-              >
-                {name}
-              </span>
-            </button>
-          );
-        })}
-      </div>
+    return (
+        <>
+            <div className="flex justify-between items-center text-white-1 font-semibold text-xl">
+                <h2 className="dark:text-white-1 text-dark-1">
+                    Connect Wallet
+                </h2>
+                <CgCloseR
+                    className="cursor-pointer dark:text-white-1 text-dark-1"
+                    onClick={onClose}
+                />
+            </div>
+            <div className="">
+                <div
+                    className={`text-gray-6 p-2 rounded md:rounded-xl w-full mt-2 mb-4 text-left`}
+                >
+                    <span className="text-base md:text-base">
+                        <span>
+                            By connecting a wallet, you have agreed to chained
+                            thrift{" "}
+                        </span>
+                        <span>
+                            <a href="#!" className="text-blue-1">
+                                terms of service
+                            </a>
+                        </span>
+                    </span>
+                </div>
+                {connectorsData.map((connectorObj, idx) => {
+                    const { name, connector, icon: Icon } = connectorObj;
+                    return (
+                        <button
+                            key={idx}
+                            className={clsx({
+                                "bg-white-1 dark:bg-gray-5 text-white-1 p-2 rounded md:rounded-xl block w-full mt-4 text-left align-middle": true,
+                                "opacity-60 cursor-not-allowed": false,
+                            })}
+                            onClick={() => connectWallet(connector)}
+                        >
+                            <Icon width="35px" className="inline-block mr-6" />
+                            <span
+                                className={`inline-block text-base dark:text-white-1 text-dark-1`}
+                            >
+                                {name}
+                            </span>
+                        </button>
+                    );
+                })}
+            </div>
 
-      <div className="mt-6 text-sm text-gray-6">
-        <p>New to Wallet?</p>
-        <a
-          href="https://ethereum.org/wallets/"
-          target="_blank"
-          rel="noreferrer"
-          className="text-blue-1"
-        >
-          Learn more about wallets
-        </a>
-      </div>
-    </>
-  );
+            <div className="mt-6 text-sm text-gray-6">
+                <p>New to Wallet?</p>
+                <a
+                    href="https://ethereum.org/wallets/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-blue-1"
+                >
+                    Learn more about wallets
+                </a>
+            </div>
+        </>
+    );
 };
 
 export { ConnectWalletModal };
--- src/providers/appProvider.jsx
+++ src/providers/appProvider.jsx
@@ -1,12 +1,16 @@
-import { Web3ReactProvider } from '@web3-react/core'
+import { Web3ReactProvider } from '@web3-react/core';
+import { ToastProvider } from 'react-toast-notifications';
+
 import React from 'react'
 import { getLibrary } from '../web3'
 
 const AppProvider = ({children}) => {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-        {children}
-    </Web3ReactProvider>
+    <ToastProvider newestOnTop = {true} autoDismiss = {true} >
+      <Web3ReactProvider getLibrary={getLibrary}>
+          {children}
+      </Web3ReactProvider>
+    </ToastProvider>
   )
 }
 
